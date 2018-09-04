c6d2aa5b1f38dc2ad5899ce8ec290c6508c696eb
Sigurdur Halldorsson <sigurdurhalldorsson@clients-pool-6.nat.ru.is>
2018-09-04 10:44:51 +0000

    init

---
+++ Hellopython.py
@@ -0,0 +1,21 @@
+nums = [1, 2, 3, 4, 5]
+
+for num in nums:
+    if num == 3:
+        print("Found")
+        break #continue
+    print(num)
+
+
+
+x = 0
+
+while x < 10:
+    if x == 5:
+        break
+    print(x)
+    x +=1
+
+
+for i in range(0, num):
+    print(i, end=" ") #sleppur því að gera nýja línu
